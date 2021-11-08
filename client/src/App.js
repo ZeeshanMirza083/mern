2b3601c46db40359cab317865a16fd7086222522
Zeeshan Mirza <zeeshanwanological@gmail.com>
2021-11-08 20:01:59 +0500

    Create MERN project with Material UI...

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography>Hello World!!!!</Typography>
+    </div>
+  );
+}
+
+export default App;

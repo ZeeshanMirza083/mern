c4124c6c6798d352c7055e361a0e4d2bd78fa9fa
Zeeshan Mirza <zeeshanwanological@gmail.com>
2021-11-09 11:43:31 +0500

    connection between react & express.....

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 4000
 
+app.use(cors())
+
+const arr = [
+    { id: 1, name: 'zeeshan', city: 'zafarwal' },
+    { id: 2, name: 'ali', city: 'narowal' },
+    { id: 3, name: 'rizwan', city: 'sadaqabad' },
+    { id: 4, name: 'gilman', city: 'test' },
+    { id: 5, name: 'rehan', city: 'test' },
+]
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.json(arr)
 })
 
 app.listen(port, () => {

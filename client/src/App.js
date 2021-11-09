0d17a6f118f20efa9e7db335c2574e2de9557e07
Zeeshan Mirza <zeeshanwanological@gmail.com>
2021-11-09 13:31:40 +0500

    add redux & routing....

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import { Typography } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import './App.css';
 
 function App() {
 
+  const [data, setData] = useState(undefined)
+
   useEffect(() => {
 
     axios.get('http://localhost:4000/')
       .then(function (response) {
-        console.log(response.data);
+        setData(response.data)
       })
       .catch(function (error) {
         console.log(error);
@@ -19,7 +21,14 @@ function App() {
 
   return (
     <div className="App">
-      <Typography>Hello World!!!!</Typography>
+      <Typography>Fetch data from 'localhost:4000'</Typography>
+      {
+        data ? data.map((item) => {
+          return <div>
+            <Typography>{item.id}-{item.name}-{item.city}</Typography>
+          </div>
+        }) : null
+      }
     </div>
   );
 }
